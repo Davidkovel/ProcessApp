be61c3fdca0eb4e79e7620fd906980ebfbbb63c9
David <dkovel7@gmail.com>
2025-04-05 11:52:53 +0100

    Task 1 is done

--- ProcessApp/obj/Debug/net8.0/ProcessApp.AssemblyInfo.cs
+++ ProcessApp/obj/Debug/net8.0/ProcessApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProcessApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8c943bf0ccf108ee64f78e6cdd886b9623bb457")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProcessApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProcessApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
